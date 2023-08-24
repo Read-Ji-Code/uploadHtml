24ce166b68e03db2af4e578b33e4ceb6834c6ca4
Read-Ji-Code <102992573+Read-Ji-Code@users.noreply.github.com>
2023-08-24 09:33:33 +0900

    자바스크립트시작

--- 02/js/02.js
+++ 02/js/02.js
@@ -1,7 +1,7 @@
 document.addEventListener("DOMContentLoaded", function() {
-    document.getElementById("ah1").innerHTML = "Hello JavaScript";
-});
+    document.getElementById("ah1").innerHTML = "Hello JavaScript!" ;
+}) ;
 
 function hello() {
-    console.log("Hello JS!")
+    console.log("Hello JS!") ;
 }
